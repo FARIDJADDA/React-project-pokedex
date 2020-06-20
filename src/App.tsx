707fc3783640fdc79d349f9011fb372f68a6dfff
djadda Farid <faridjadda750@gmail.com>
2020-06-21 01:38:15 +0200

    update index.html App.tsx pokemon-form.tsx pokemon-detail.tsx pokemon-detail.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
+import PokemonEdit from "./pages/pokemon-edit";
 import PokemonsDetail from "./pages/pokemon-detail";
 import PokemonList from "./pages/pokemon-list";
 import PageNotFound from "./pages/page-not-found";
@@ -21,6 +22,7 @@ const App: FunctionComponent = () => {
         <Switch>
           <Route exact path="/" component={PokemonList} />
           <Route exact path="/pokemons" component={PokemonList} />
+          <Route path="/pokemons/edit/:id" component={PokemonEdit} />
           <Route path="/pokemons/:id" component={PokemonsDetail} />
           <Route component={PageNotFound} />
         </Switch>
