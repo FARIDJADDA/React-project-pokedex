fb52deaba2f9486d9eafb71b9a7b4a48e7d1323d
djadda Farid <faridjadda750@gmail.com>
2020-06-30 12:47:08 +0200

    deploy application with firebase

---
+++ build/precache-manifest.fa223478f8801e194fc63fa02767f9b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cca909ad690532b94a281e800b2563cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d82f2c67b6f3037964d",
+    "url": "/static/css/main.3894da33.chunk.css"
+  },
+  {
+    "revision": "a493daaf6ba1dc71dad6",
+    "url": "/static/js/2.0412893c.chunk.js"
+  },
+  {
+    "revision": "7d82f2c67b6f3037964d",
+    "url": "/static/js/main.9093f59a.chunk.js"
+  },
+  {
+    "revision": "d2fce7ee7a4e8a43e9a6",
+    "url": "/static/js/runtime-main.d42f56a1.js"
+  },
+  {
+    "revision": "864b1c15a8818e9c38b5681aa4baed50",
+    "url": "/static/media/dracaufeu.864b1c15.png"
+  }
+]);
--- src/services/pokemon-service.ts
+++ src/services/pokemon-service.ts
@@ -1,62 +1,116 @@
 import Pokemon from "../models/pokemon";
+import POKEMONS from "../models/mock-pokemon";
 
 export default class PokemonService {
+  static pokemons: Pokemon[] = POKEMONS;
+
+  static isDev =
+    !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+  // ---------------GET-REQUEST-----------------------------
+
   static getPokemons(): Promise<Pokemon[]> {
-    return fetch("http://localhost:3001/pokemons").then((response) =>
-      response.json().catch((error) => this.handleError(error))
-    );
+    if (this.isDev) {
+      return fetch("http://localhost:3001/pokemons")
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+
+    return new Promise((resolve) => {
+      resolve(this.pokemons);
+    });
   }
 
   static getPokemon(id: number): Promise<Pokemon | null> {
-    return fetch(`http://localhost:3001/pokemons/${id}`)
-      .then((response) => response.json())
-      .then((data) => (this.isEmpty(data) ? null : data))
-      .catch((error) => this.handleError(error));
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/pokemons/${id}`)
+        .then((response) => response.json())
+        .then((data) => (this.isEmpty(data) ? null : data))
+        .catch((error) => this.handleError(error));
+    }
+
+    return new Promise((resolve) => {
+      resolve(this.pokemons.find((pokemon) => id === pokemon.id));
+    });
   }
+
   // ----------------PUT-REQUEST-----------------------------
 
   static updatePokemon(pokemon: Pokemon): Promise<Pokemon> {
-    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
-      method: "PUT", // type de la requete à envoyer
-      body: JSON.stringify(pokemon), // defini le corp de la requete (transofrme un objet en chaine de character stringify)
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+        method: "PUT",
+        body: JSON.stringify(pokemon),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+
+    return new Promise((resolve) => {
+      const { id } = pokemon;
+      const index = this.pokemons.findIndex((pokemon) => pokemon.id === id);
+      this.pokemons[index] = pokemon;
+      resolve(pokemon);
+    });
   }
+
   // ----------------DELETE-REQUEST------------------------------
 
   static deletePokemon(pokemon: Pokemon): Promise<{}> {
-    return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/pokemons/${pokemon.id}`, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+
+    return new Promise((resolve) => {
+      const { id } = pokemon;
+      this.pokemons = this.pokemons.filter((pokemon) => pokemon.id !== id);
+      resolve({});
+    });
   }
 
   // ----------------POST-REQUEST---------------------------------
 
   static addPokemon(pokemon: Pokemon): Promise<Pokemon> {
-    delete pokemon.created;
-    return fetch(`http://localhost:3001/pokemons`, {
-      method: "POST",
-      body: JSON.stringify(pokemon),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
+    pokemon.created = new Date(pokemon.created);
+
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/pokemons`, {
+        method: "POST",
+        body: JSON.stringify(pokemon),
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
+
+    return new Promise((resolve) => {
+      this.pokemons.push(pokemon);
+      resolve(pokemon);
+    });
   }
 
   //  -------------------SEARCH-----------------------------------
 
   static searchPokemon(term: string): Promise<Pokemon[]> {
-    // retourne un promesse avec un tableau de pokemon
-    return fetch(`http://localhost:3001/pokemons?q=${term}`) // Filtre les ppokemons d'apres leurs nom en fonction d'un term de recherche entré par l'utilisateur
-      .then((response) => response.json())
-      .catch((error) => this.handleError(error));
-  }
+    if (this.isDev) {
+      return fetch(`http://localhost:3001/pokemons?q=${term}`)
+        .then((response) => response.json())
+        .catch((error) => this.handleError(error));
+    }
 
+    return new Promise((resolve) => {
+      const results = this.pokemons.filter((pokemon) =>
+        pokemon.name.includes(term)
+      );
+      resolve(results);
+    });
+  }
   // -------------------------------------------------------------
 
   static isEmpty(data: Object): boolean {
